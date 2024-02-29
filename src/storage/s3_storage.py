fef653cf8c520c56130a5be382c22e2b0e78605a
Michael <moreno.m2698@gmail.com>
2024-02-29 18:24:48 -0500

    added s3 client to s3_storage

--- src/storage/s3_storage.py
+++ src/storage/s3_storage.py
@@ -1,12 +1,13 @@
-from src.storage.storage_abc import Storage_ABC
-
+from .storage_abc import Storage_ABC
+import boto3
+from botocore.exceptions import ClientError
 
 
 
 class S3_Storage(Storage_ABC):
 
     def __init__(self):
-        pass
+        self.client = boto3.client("s3")
 
     def retrieve(self):
         print('retrieve')
